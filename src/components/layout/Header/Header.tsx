82659f24507f480868ca0df931b5cd70760d7ae5
Viraj <viraj.nirbhavane@brello.co.in>
2026-03-16 21:02:32 +0530

    fix for undefined charAt in Header lastPath

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -30,7 +30,9 @@ export const Header: React.FC<HeaderProps> = ({ toggleSidebar, isSidebarCollapse
 
   const pathname = location.pathname.split('/').filter(Boolean);
   const lastPathname = pathname[pathname.length - 1];
-  const breadcrumbText = lastPathname.charAt(0).toUpperCase() + lastPathname.slice(1);
+  const breadcrumbText = lastPathname
+    ? lastPathname.charAt(0).toUpperCase() + lastPathname.slice(1)
+    : 'Home';
 
   const profileActionItems = [
     ...(!isDesktop
