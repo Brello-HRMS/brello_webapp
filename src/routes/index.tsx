ded283a8c05461746a075f5a6c34d3eb6a55e620
Mohd Samir <samirmohd701@gmail.com>
2026-03-29 21:49:36 +0530

    fix:route merge conflict

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -144,6 +144,8 @@ const router = createBrowserRouter([
       {
         path: 'attendance/holidays/:id',
         element: <HolidayCalendarView />,
+      },
+      {
         path: 'project/projects',
         element: <ProjectPage />,
       },
